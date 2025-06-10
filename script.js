5d7350777d106104b6166f2a0b60004aa7031a23
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 15:28:34 -0700

    Create calculate function

---
+++ script.js
@@ -0,0 +1,17 @@
+function operate(a, b, operator) {
+    switch(operater) {
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break;
+        case "x":
+            return a * b;
+            break;
+        case "/":
+            return a/b;
+            break;
+    }
+}
+
