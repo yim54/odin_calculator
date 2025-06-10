86f380ce1d4c96cae937d3e0a84ff93278a78888
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 16:17:38 -0700

    Add text display when pressing buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function operate(a, b, operator) {
+function calculate(a, b, operator) {
     switch(operater) {
         case "+":
             return a + b;
@@ -15,3 +15,27 @@ function operate(a, b, operator) {
     }
 }
 
+let display =  document.querySelector("#display");
+let number = "";
+let operator = "";
+
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    display.textContent = "";
+});
+
+let numbers = document.querySelectorAll(".number");
+for (let i = 0; i < numbers.length; i++)  {
+    numbers[i].addEventListener("click", (event) => {
+        display.textContent += event.target.textContent;
+        number += event.target.textContent;
+    })
+}
+
+let operators = document.querySelectorAll(".operator");
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", (event) => {
+        display.textContent = "";
+        operator = event.target.textContent;
+    })
+}
