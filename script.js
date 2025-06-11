568d2e262e0453fb2cc4595c83680d3513b1e35e
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 17:21:05 -0700

    Fix edge cases

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let display =  document.querySelector("#display");
+let num1 = "";
+let num2 = "";
+let operator = "";
+let reset = false; // determines if next input should clear screen
+
 function calculate(a, b, operator) {
-    switch(operater) {
+    a = Number(a);
+    b = Number(b);
+    switch(operator) {
         case "+":
             return a + b;
             break;
@@ -10,44 +18,79 @@ function calculate(a, b, operator) {
             return a * b;
             break;
         case "/":
-            return a/b;
+            return Math.round(a/b*1000)/1000;
             break;
     }
 }
 
-let display =  document.querySelector("#display");
-let num1 = "";
-let num2 = "";
-let operator = "";
+function clear() {
+    num1 = "";
+    num2 = "";
+    operator = "";
+}
 
-let clear = document.querySelector(".clear");
-clear.addEventListener("click", () => {
+let clearBut = document.querySelector(".clear");
+clearBut.addEventListener("click", () => {
     display.textContent = "";
+    clear();
 });
 
 let numbers = document.querySelectorAll(".number");
 for (let i = 0; i < numbers.length; i++)  {
     numbers[i].addEventListener("click", (event) => {
-        display.textContent += event.target.textContent;
+        if (reset) {
+            // clear screen first
+
+            display.textContent = event.target.textContent;
+            reset = false;
+        }
+        else {
+            display.textContent += event.target.textContent;
+        }
     })
 }
 
 let operators = document.querySelectorAll(".operator");
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", (event) => {
+        // check if operator is alread set
+
+        if (operator != "") {
+            operator = event.target.textContent;
+            return;
+        }
+
+
         if (num1 == "") {
             num1 = display.textContent;
+            display.textContent = "";
+            operator = event.target.textContent;
         } else {
+            // perform calculation
+
             num2 = display.textContent;
+            console.log(num1 + operator + num2);
+            display.textContent = calculate(num1, num2, operator);
+
+            // set values for next calculation
+
+            num1 = display.textContent;
+            operator = event.target.textContent;
+            reset = true;
         }
-        display.textContent = "";
-        operator = event.target.textContent;
     })
 }
 
 let equal = document.querySelector(".equal");
 equal.addEventListener("click", () => {
-    if (num1 != "" & num2 != "") {
+    // check if there is a full expression
+
+    if (num1 != "" & display.textContent != "") {
+        // perform calculation;
+
+        num2 = display.textContent;
         display.textContent = calculate(num1, num2, operator);
+        clear();
+        reset = true;
     }
 })
